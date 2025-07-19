dd3e34070cef7df34cac6c88be74900f13cf4424
Bharadwaj07 <yajamankr@gmail.com>
2025-07-19 20:01:17 +0530

    FEAT:Updated reame.md

--- cache/redisClient.js
+++ cache/redisClient.js
@@ -1,8 +1,9 @@
 // src/cache/redisClient.js
 const { createClient } = require('redis');
 
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 const redis = createClient({
-  url: 'redis://localhost:6379',
+  url: REDIS_URL,
 });
 
 redis.on('error', (err) => {
